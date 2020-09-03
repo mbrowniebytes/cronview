aa2ceff67ca54891c69223ecd93d712d9ba8825d
Michael Brown <mbrown@sciusa.com>
2020-09-03 16:17:56 -0500

    check for empty lines; check for common crontab prefix commands

--- cronview.py
+++ cronview.py
@@ -12,21 +12,26 @@ def main(args):
     try:
         for line in sys.stdin:
             li=line.strip()
-            if not li.startswith("#"):
-                mycron = line.rstrip()
-                mycronlist = mycron.split()
-                mycrondate = ' '.join(mycronlist[0:5])
-                cron = croniter.croniter(mycrondate, now)
-                for i in range(0,int(sys.argv[1])): # get each cronjob n times, not optimal
-                    x = cron.get_next(datetime.datetime).strftime(DATEFORMAT)
-                    y = str(' '.join(mycronlist[5::]))
-                    cronlist.append((x, y)) # list of tuples
+            if li == '':
+                continue
+            if li.startswith('SHELL') or li.startswith('PATH') or li.startswith('MAILTO'):    
+                continue
+            if li.startswith("#"):
+                continue
+            mycron = line.rstrip()
+            mycronlist = mycron.split()
+            mycrondate = ' '.join(mycronlist[0:5])
+            cron = croniter.croniter(mycrondate, now)
+            for i in range(0,int(sys.argv[1])): # get each cronjob n times, not optimal
+                x = cron.get_next(datetime.datetime).strftime(DATEFORMAT)
+                y = str(' '.join(mycronlist[5::]))
+                cronlist.append((x, y)) # list of tuples
 
         cronlist_sorted = sorted(cronlist, key=lambda x: datetime.datetime.strptime(x[0], DATEFORMAT))
         for i in cronlist_sorted[:int(sys.argv[1])]:
             print(i[0] + ' - ' + i[1])
     except IndexError:
-        print("You need to specify how many occurences, ie. crontab -l | python view.py 4")
+        print("You need to specify how many occurrences, ie. crontab -l | python view.py 4")
 
 
 if __name__ == '__main__':
